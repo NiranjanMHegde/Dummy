3c3f475b5d8941d9309bb7d9aa8c2597aec019e4
Niranjan_Hegde <niranjanhegde16@gmail.com>
2023-11-16 12:22:07 +0530

    Question 3

--- example.py
+++ example.py
@@ -13,20 +13,42 @@
 
 #Second Question
 
-class Shape:
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
+# class Shape:
+#     def __init__(self,width,height):
+#         self.width = width
+#         self.height = height
 
-    def area(self):
-         return 0
+#     def area(self):
+#          return 0
     
-class Rectangle(Shape):
-     def area(self):
-          return (self.width * self.height)
+# class Rectangle(Shape):
+#      def area(self):
+#           return (self.width * self.height)
      
-r1=Rectangle(3,4)
-print(r1.area())
+# r1=Rectangle(3,4)
+# print(r1.area())
+
+
+#Question 3
+
+class Vehicle:
+    def start_engine(self):
+        print("Engine Started")
+
+class car(Vehicle):
+    def start_engine(self):
+        print("car engine started")
+    
+class motorcycle(Vehicle):
+    def start_engine(self):
+        print("motorcycle engine started")
+
+a=Vehicle()
+b=car()
+c=motorcycle()
+a.start_engine()
+b.start_engine()
+c.start_engine()
 
     
           
