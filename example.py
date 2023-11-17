33fbbc2ed78bfac5b6782ab5e88cfb8e56667074
Niranjan_Hegde <niranjanhegde16@gmail.com>
2023-11-17 10:54:07 +0530

    add programs related to classes problems

--- example.py
+++ example.py
@@ -31,24 +31,46 @@
 
 #Question 3
 
-class Vehicle:
-    def start_engine(self):
-        print("Engine Started")
+# class Vehicle:
+#     def start_engine(self):
+#         print("Engine Started")
 
-class car(Vehicle):
-    def start_engine(self):
-        print("car engine started")
+# class car(Vehicle):
+#     def start_engine(self):
+#         print("car engine started")
     
-class motorcycle(Vehicle):
-    def start_engine(self):
-        print("motorcycle engine started")
+# class motorcycle(Vehicle):
+#     def start_engine(self):
+#         print("motorcycle engine started")
 
-a=Vehicle()
-b=car()
-c=motorcycle()
-a.start_engine()
-b.start_engine()
-c.start_engine()
+# a=Vehicle()
+# b=car()
+# c=motorcycle()
+# a.start_engine()
+# b.start_engine()
+# c.start_engine()
 
     
-          
+#Question 4
+
+class Shape:
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+        
+    def area(self):
+        return (self.width * self.height)
+
+class Rectangle(Shape):
+    def area(self):
+        return (self.width * self.height)
+    
+class Triangle(Shape):
+    def area(self,base):
+        self.base=base
+        return ((self.base*self.height)*0.5)
+    
+a=Rectangle(8,4)
+b=Triangle(6,9)
+print(a.area())
+print(b.area(7))
